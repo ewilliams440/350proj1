07e43584312906431e473a4085bd243ef6845825
Kelly Williams <kwill142@binghamton.edu>
2025-03-01 16:50:21 -0500

    shutdown implement

--- shutdown.c
+++ shutdown.c
@@ -1,15 +1,22 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "x86.h"
 
 /* ATTENTION: to ensure correct compilation of the base code, 
    stub functions for the system call user space wrapper functions are provided. 
    REMEMBER to disable the stub functions (by commenting the following macro) to 
-   allow your implementation to work properly. */
+   allow your implementation to work properly.
 #define STUB_FUNCS
 #ifdef STUB_FUNCS
 void shutdown(void) {}
 #endif
+*/
+
+void shutdown(void){
+	outw(0xB004, 0x0|0x2000);
+	outw(0x604, 0x0|0x2000);
+}
 
 
 /* IMPORTANT INSTRUCTION: the code below should not be changed. 
