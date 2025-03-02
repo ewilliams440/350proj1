73957575c2862b20c8c4aff8ac5ef5c9c2b9c960
Eli Williams <112526609+ewilliams440@users.noreply.github.com>
2025-03-02 18:28:39 -0500

    fixing shutdown2

--- sysproc.c
+++ sysproc.c
@@ -107,3 +107,15 @@ sys_exit2(void)
   exit();
   return 0;  
 }
+
+void sys_shutdown2(void){
+  	char *msg;
+
+  	if(argstr(0, &msg) < 0){
+    		return -1;
+  	}
+  	cprintf("%s\n",msg);
+
+	outw(0xB004, 0x0|0x2000);
+	outw(0x604, 0x0|0x2000);
+}
